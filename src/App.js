926cb216fb0e0cd4e6887b9ddd3f3c8b60590d42
Pavan Mundada <pavanpmundada5793@gmail.com>
2023-04-21 10:19:54 +0530

    added event handlers for pwa

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const styles = {
+  width:'100px',
+  height:'50px',
+  textAlign: 'center',
+  position: 'absolute',
+  top:0,
+  left:0,
+  transform: 'translate(50%,50%)'
 }
+
+const App = () => {
+  const [supportsPWA, setSupportsPWA] = useState(false);
+  const [promptInstall, setPromptInstall] = useState(null);
+
+  useEffect(() => {
+          console.log('in use')// I see in the console
+      const handler = (e) => {
+          console.log('in handler')
+          setSupportsPWA(true);// I do not see in the console
+          // e.preventDefault();
+          setPromptInstall(e);
+      };
+      window.addEventListener("beforeinstallprompt", handler(), false)
+      return () => window.removeEventListener("transitionend", handler);
+      });
+      const onClick = e => {
+          alert('click')
+          // e.preventDefault();
+          console.log('promptInstall',promptInstall)
+          if (promptInstall) {
+              promptInstall.prompt();
+          }else{
+                  return;
+          };
+      };
+
+      return (
+          supportsPWA && (<button style={styles} onClick={(e) => onClick(e)}>Install</button>)
+      )
+};
 
 export default App;
